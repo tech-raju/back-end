d3923223b1ba673833924169f7bda7ddc5ffc548
tech-raju <manjhiraju007@gmail.com>
2024-08-22 18:16:17 +0530

    correct mistek

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -3,7 +3,7 @@ class apiError extends Error {
     statusCode,
     message= "Somthing went wrong",
     errors = [],
-    statck = ""
+    stack = ""
   ){
     super(message),
     this.statusCode = statusCode,
@@ -11,10 +11,10 @@ class apiError extends Error {
     this.message = message,
     this.errors =errors
 
-    if(statck){
-      this.stack = statck
+    if(stack){
+      this.stack = stack
     }else{
-      Error.captureStackTrace(this,this.constructor)
+      Error.captureStackTrace(this, this.constructor)
     }
   }
 }
